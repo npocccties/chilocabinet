e998dbdb41ad6af373bfece211251ef5e0d41ba8
t_koga <t_koga@dev-cabinet.oku.cccties.org>
2023-12-07 12:55:36 +0900

    デバッグ用ビルドスクリプト修正

--- script/db-seed-dev-local.sh
+++ script/db-seed-dev-local.sh
@@ -4,5 +4,4 @@ DIR=$(cd $(dirname $0)/..; pwd)
 echo $DIR
 cd $DIR
 
-docker compose -f docker-compose.dev-local.yml up -d && \
 docker container exec -it chilocabinet-app sh -c 'npx prisma db seed'
