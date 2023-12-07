45ad3effaed2ee05a880d719e5528add9fbf1924
t_koga <t_koga@dev-cabinet.oku.cccties.org>
2023-12-07 12:56:15 +0900

    デバッグ用DBデータ設定修正

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,6 +7,16 @@ const prisma = new PrismaClient();
 
 async function main() {
 
+  try{
+    const [dbResult1, dbResult2] = await prisma.$transaction([
+      prisma.users.deleteMany({}),
+      prisma.submittedBadges.deleteMany({}),
+    ]);
+  }
+  catch(e) {
+    // no act
+  }
+
   const userlist = UserIDs_Test_1;
   const badgelist = SubmittedBadges_Test_1;
 
