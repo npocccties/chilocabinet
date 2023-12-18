c08b3c7f12b9b83c69a9aba11a55ea5b178f49f1
t_koga <t_koga@dev-cabinet.oku.cccties.org>
2023-12-18 13:34:37 +0900

    コンテナ起動用のスクリプトファイルを調整

---
+++ app_start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# バックエンドのコンテナ起動
+echo $0
+DIR=$(cd $(dirname $0); pwd)
+echo $DIR
+cd $DIR
+if ! docker network ls | grep db_network > /dev/null 2>&1; then
+    docker network create db_network
+fi
+docker compose -f docker-compose.production-db.yml up -d
+docker container stop chilocabinet 2>&1 || true
+docker compose -f docker-compose.production.yml up -d --build
+/bin/sh ./server_db_restore.sh
