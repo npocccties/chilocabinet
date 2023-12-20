76b9802bfea695fff1e75e7c3ff8353c0d382033
t_koga <t_koga@dev-cabinet.oku.cccties.org>
2023-12-20 10:01:38 +0900

    ビルド警告対策：nextjsバージョンアップ,設定調整

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputStandalone: true,
-  },
+  output: "standalone",
 };
 
 module.exports = nextConfig;
