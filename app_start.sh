0cdbe209ee2d849a625fa576c1d1bc2cabc6a251
t_koga <t_koga@dev-cabinet.oku.cccties.org>
2023-12-20 10:03:52 +0900

    ビルド警告対策：スクリプト修正,docker compose の呼び出しパラメータ変更

--- app_start.sh
+++ app_start.sh
@@ -7,7 +7,13 @@ cd $DIR
 if ! docker network ls | grep db_network > /dev/null 2>&1; then
     docker network create db_network
 fi
+
 docker compose -f docker-compose.production-db.yml up -d
-docker container stop chilocabinet 2>&1 || true
-docker compose -f docker-compose.production.yml -p chilocabinet  up -d  --build 
+
+if docker ps -q --filter "name=^chilocabinet$" | grep -q .; then
+    echo "Stopping existing container: chilocabinet"
+    docker container stop chilocabinet
+fi
+
+docker compose -f docker-compose.production.yml -p chilocabinet up -d --build
 /bin/sh ./server_db_restore.sh
